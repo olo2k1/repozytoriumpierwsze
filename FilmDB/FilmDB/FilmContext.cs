9a366751513298c0be8d8b739dd9dbc471d6a8cb
Aleksander Kaska <s9752ak@ms.wwsi.edu.pl>
2022-11-05 17:08:38 +0100

    all

---
+++ FilmDB/FilmDB/FilmContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmDB
+{
+  public class FilmContext : DbContext
+    {
+        public DbSet<FilmModel> Films { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptions)
+        {
+            var cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FilmDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+
+            dbContextOptions.UseSqlServer(cs);
+        }
+    }
+}
